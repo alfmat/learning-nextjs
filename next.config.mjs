0973a5054434932bfda048ae0b82bb395c3caa49
Alfred Mathew <alfredmathew718@gmail.com>
2024-09-17 22:16:28 -0400

    adding some pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import tailwindForms from '@tailwindcss/forms';
+
+const nextConfig = {
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'tailwindui.com',
+                pathname: '/img/**',
+            }
+        ]
+    }, 
+    // plugins: [
+    //     tailwindForms,
+    // ],
+};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Header from "@/app/ui/home/header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full bg-white">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`h-full`}
       >
+        <div>
+          <Header/>
+        </div>
         {children}
       </body>
     </html>
